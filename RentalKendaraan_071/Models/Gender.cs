6a46aa5cc7a9fad4df5c22c0357a84e20f812735
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-10-26 18:36:46 +0700

    add cotroller

---
+++ RentalKendaraan_071/Models/Gender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_071.Models
+{
+    public partial class Gender
+    {
+        public int IdGender { get; set; }
+        public string NamaGender { get; set; }
+    }
+}
---
+++ RentalKendaraan_071/Models/Peminjaman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_071.Models
+{
+    public partial class Peminjaman
+    {
+        public int IdPeminjaman { get; set; }
+        public DateTime? TgiPeminjaman { get; set; }
+        public int? IdKendaraan { get; set; }
+        public int? IdCustomer { get; set; }
+        public int? IdJaminan { get; set; }
+        public int? Biaya { get; set; }
+    }
+}
