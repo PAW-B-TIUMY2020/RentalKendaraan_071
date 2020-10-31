d198f62eeb006088d20ebe43b04a3eda97d0c614
AisyahDiniaKencana <56532105+AisyahDiniaKencana@users.noreply.github.com>
2020-10-31 12:58:56 +0700

    lagi

--- RentalKendaraan_071/Models/Peminjaman.cs
+++ RentalKendaraan_071/Models/Peminjaman.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace RentalKendaraan_071.Models
 {
     public partial class Peminjaman
     {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+        [DisplayName("ID")]
         public int IdPeminjaman { get; set; }
-        public DateTime? TgiPeminjaman { get; set; }
+        [DisplayName("Tanggal Peminjaman")]
+        public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
         public int? IdKendaraan { get; set; }
-        public int? IdCustomer { get; set; }
+        [DisplayName("Customer")]
+        public int? IdCustumer { get; set; }
+        [DisplayName("Jaminan")]
         public int? IdJaminan { get; set; }
         public int? Biaya { get; set; }
+        [DisplayName("Customer")]
+        public Customer IdCostumerNavigation { get; set; }
+        [DisplayName("Jaminan")]
+        public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
     }
 }
